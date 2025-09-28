0cfd8f1ab2f740c38936d78123c8a7add68a1afc
SahilDhanwani <sahildhanwani291203@gmail.com>
2025-09-28 18:49:42 +0530

    fix: Update PhotoEditorAPIBaseURL to use the correct domain in production environment

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    ChatAppAPIBaseURL: 'https://api.chat-zone.tech:8443',
-    PhotoEditorAPIBaseURL: 'https://api.chat-zone.tech'
+    ChatAppAPIBaseURL: 'https://api.chat-zone.tech',
+    PhotoEditorAPIBaseURL: 'https://chat-zone.tech/photo-editor'
 };
